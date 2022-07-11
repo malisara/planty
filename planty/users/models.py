55848caf72e63616a950af5893ba691bf7e34502
Sara Mali <mali.sara7@gmail.com>
2022-07-11 14:57:18 +0200

    added review model

--- planty/users/models.py
+++ planty/users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from planty.utils import save_cropped_image
+from datetime import datetime
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Profile(models.Model):
@@ -13,4 +15,10 @@ class Profile(models.Model):
         save_cropped_image(self.image.path, 200)
 
 
-# TODO Reviews model
+class Reviews(models.Model):
+    comment_author = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment_reciever = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    review = models.CharField(max_length=250)
+    rating = models.IntegerField(default=5, validators=[
+                                 MinValueValidator(1), MaxValueValidator(5)])
+    date = models.DateTimeField(default=datetime.now)
